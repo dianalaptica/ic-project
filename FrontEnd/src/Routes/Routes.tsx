b830a7de4c0ba6dbc57f7beb10828fee9809477e
dianalaptica <dianalaptica@gmail.com>
2024-04-16 14:03:20 +0300

    FE: Added protected routes.

--- FrontEnd/src/Routes/Routes.tsx
+++ FrontEnd/src/Routes/Routes.tsx
@@ -3,6 +3,8 @@ import Login from "../Components/Login/Login";
 import Register from "../Components/Register/Register";
 import Dashboard from "../Components/Dashboard/Dashboard";
 import App from "../App";
+import ProtectedRoute from "./ProtectedRoute";
+import Admin from "../Components/Admin/Admin";
 
 // create a router
 export const router = createBrowserRouter([
@@ -12,7 +14,22 @@ export const router = createBrowserRouter([
     children: [
       { path: "", element: <Login /> },
       { path: "register", element: <Register /> },
-      { path: "dashboard", element: <Dashboard /> },
+      {
+        path: "dashboard",
+        element: (
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "admin",
+        element: (
+          <ProtectedRoute>
+            <Admin />
+          </ProtectedRoute>
+        ),
+      },
     ],
   },
 ]);
