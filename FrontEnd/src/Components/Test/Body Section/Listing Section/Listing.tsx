c43b029b3acf13aeacc44594ee29d7d87330513c
dianalaptica <dianalaptica@gmail.com>
2024-05-25 17:00:09 +0300

    Added trips cards.

--- FrontEnd/src/Components/Test/Body Section/Listing Section/Listing.tsx
+++ FrontEnd/src/Components/Test/Body Section/Listing Section/Listing.tsx
@@ -1,7 +1,49 @@
 import React from "react";
+import "./Listing.css";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import { BsArrowRightShort } from "react-icons/bs";
+import paris from "../../../../LoginAssets/paris.png";
+import london from "../../../../LoginAssets/london.png";
+import rome from "../../../../LoginAssets/rome.png";
+import berlin from "../../../../LoginAssets/berlin.png";
 
 const Listing = () => {
-  return <div>Listing</div>;
+  return (
+    <div className="listingSection">
+      <div className="heading flex">
+        <h1>See trips from popular locations</h1>
+        <button className="btn flex">
+          See All <BsArrowRightShort className="icon" />
+        </button>
+      </div>
+
+      <div className="secContainer flex">
+        <div className="singleItem">
+          <AiFillHeart className="icon" />
+          <img src={paris} alt="Eiffel Tower" />
+          <h3>Paris, FR</h3>
+        </div>
+
+        <div className="singleItem">
+          <AiOutlineHeart className="icon" />
+          <img src={london} alt="Big Ben" />
+          <h3>London, UK</h3>
+        </div>
+
+        <div className="singleItem">
+          <AiOutlineHeart className="icon" />
+          <img src={rome} alt="Colosseum" />
+          <h3>Rome, IT</h3>
+        </div>
+
+        <div className="singleItem">
+          <AiFillHeart className="icon" />
+          <img src={berlin} alt="Berlin" />
+          <h3>Berlin, D</h3>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Listing;
