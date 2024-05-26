314c68b8727a29c99d328e5f3db3c04fdf0603a4
dianalaptica <dianalaptica@gmail.com>
2024-05-26 12:36:51 +0300

    Added route for guide and modified routes.

--- FrontEnd/src/Routes/GuideRoute.tsx
+++ FrontEnd/src/Routes/GuideRoute.tsx
@@ -4,21 +4,22 @@ import { useAuth } from "../Context/useAuth";
 
 type Props = { children: React.ReactNode };
 
-const TouristRoute = ({ children }: Props) => {
+const GuideRoute = ({ children }: Props) => {
   const location = useLocation();
   const { user } = useAuth();
 
-  if (user?.role === "Tourist") {
+  if (user?.role === "Guide") {
     return <>{children}</>;
   } else if (user?.role === "Admin") {
     return <Navigate to="/admin" state={{ from: location }} replace />;
-  } else if (user?.role === "Guide") {
+  } else if (user?.role === "Tourist") {
     return (
-      <Navigate to="/guide/dashboard" state={{ from: location }} replace />
+      <Navigate to="/tourist/dashboard" state={{ from: location }} replace />
     );
   } else {
+    // Fallback in case the role doesn't match any of the expected roles
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 };
 
-export default TouristRoute;
+export default GuideRoute;
--- FrontEnd/src/Routes/Routes.tsx
+++ FrontEnd/src/Routes/Routes.tsx
@@ -1,13 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "../Components/Login/Login";
 import Register from "../Components/Register/Register";
-import Dashboard from "../Components/Dashboard/Dashboard";
+import Dashboard from "../Components/Dashboard OLD/DashboardOLD";
 import App from "../App";
 import ProtectedRoute from "./ProtectedRoute";
 import Admin from "../Components/Admin/Admin";
 import AdminRoute from "./AdminRoute";
 import TouristRoute from "./TouristRoute";
 import Test from "../Components/Test/Test";
+import DashboardTourist from "../Components/Tourist/DashboardTourist/DashboardTourist";
+import GuideRoute from "./GuideRoute";
 
 // create a router
 export const router = createBrowserRouter([
@@ -17,7 +19,26 @@ export const router = createBrowserRouter([
     children: [
       { path: "", element: <Login /> },
       { path: "register", element: <Register /> },
-      { path: "test", element: <Test /> },
+      {
+        path: "guide/dashboard",
+        element: (
+          <ProtectedRoute>
+            <GuideRoute>
+              <Test />
+            </GuideRoute>
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "tourist/dashboard",
+        element: (
+          <ProtectedRoute>
+            <TouristRoute>
+              <DashboardTourist />
+            </TouristRoute>
+          </ProtectedRoute>
+        ),
+      },
       {
         path: "dashboard",
         element: (
