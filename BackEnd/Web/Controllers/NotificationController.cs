cea920261577daf4053752085474101a479d1f34
dianalaptica <dianalaptica@gmail.com>
2024-05-28 16:46:02 +0300

    Refactored controllers to send proper error message to FE.

--- BackEnd/Web/Controllers/NotificationController.cs
+++ BackEnd/Web/Controllers/NotificationController.cs
@@ -22,65 +22,96 @@ public class NotificationController : ControllerBase
     [Authorize(Roles = "Guide")]
     public async Task<ActionResult<TripNotification>> CreateNotification([FromBody] NotificationCreateDto notification)
     {
-        var result = await _notificationService.CreateNotificationAsync(notification);
-        if (result is not null)
+        try
         {
-            return CreatedAtAction(nameof(GetNotification), new { id = result.Id }, result);
+            var result = await _notificationService.CreateNotificationAsync(notification);
+            if (result is not null)
+            {
+                return CreatedAtAction(nameof(GetNotification), new { id = result.Id }, result);
+            }
+            return BadRequest();
+        } catch
+        {
+            return StatusCode(500, "Error connecting to Database.");
         }
-
-        return BadRequest();
+        
     }
 
     [HttpGet("{id}")]
     [Authorize(Roles = "Guide, Tourist")]
     public async Task<ActionResult<TripNotification>> GetNotification(int id)
     {
-        var notification = await _notificationService.GetNotificationByIdAsync(id, false);
-        if (notification == null)
+        try
         {
-            return NotFound();
+            var notification = await _notificationService.GetNotificationByIdAsync(id, false);
+            if (notification == null)
+            {
+                return NotFound();
+            }
+            return Ok(notification);
         }
-
-        return Ok(notification);
+        catch
+        {
+            return StatusCode(500, "Error connecting to Database.");
+        }
     }
     
     [HttpGet("user")]
     [Authorize(Roles = "Tourist")]
     public async Task<ActionResult<UserNotificationResponseDto>> GetUsersNotification()
     {
-        var notifications = await _notificationService.GetNotificationsByUserIdAsync(false);
-        if (notifications == null || !notifications.Any())
+        try
         {
-            return NotFound();
+            var notifications = await _notificationService.GetNotificationsByUserIdAsync(false);
+            if (notifications == null || !notifications.Any())
+            {
+                return NotFound();
+            }
+            return Ok(notifications);
         }
-
-        return Ok(notifications);
+        catch
+        {
+            return StatusCode(500, "Error connecting to Database.");
+        }
     }
     
     [HttpPatch("user/{id}")]
     [Authorize(Roles = "Tourist")]
     public async Task<ActionResult<TripNotification>> UpdateReadStatus([FromRoute]int id)
     {
-        var notification = await _notificationService.UpdateReadStatusAsync(id, true);
-        if (notification == null)
+        try
         {
-            return NotFound();
+            var notification = await _notificationService.UpdateReadStatusAsync(id, true);
+            if (notification == null)
+            {
+                return NotFound();
+            }
+            return Ok(notification);
         }
-
-        return Ok(notification);
+        catch
+        {
+            return StatusCode(500, "Error connecting to Database.");
+        }
     }
     
     [HttpDelete("{id}")]
     [Authorize(Roles = "Guide")]
     public async Task<ActionResult<TripNotification>> DeleteNotification([FromRoute]int id)
     {
-        var notification = await _notificationService.DeleteNotificationAsync(id);
-        if (notification == null)
+        try
         {
-            return BadRequest();
-        }
+            var notification = await _notificationService.DeleteNotificationAsync(id);
+            if (notification == null)
+            {
+                return BadRequest();
+            }
 
-        return NoContent();
+            return NoContent();
+        }
+        catch
+        {
+            return StatusCode(500, "Error connecting to Database.");
+        }
     }
     
     // TODO: THERE MIGHT BE NICE TO ADD MORE VALIDATION VIA JWT TOKEN
