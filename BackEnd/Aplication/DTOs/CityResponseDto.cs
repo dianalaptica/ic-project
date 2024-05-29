6117392805f6b2dd4b4115d1bdfff6e76b8e8c5f
Gherasim Robert <ghera.robi@gmail.com>
2024-05-29 19:57:44 +0300

    Added city controller

---
+++ BackEnd/Aplication/DTOs/CityResponseDto.cs
@@ -0,0 +1,8 @@
+namespace BackEnd.Aplication.DTOs;
+
+public class CityResponseDto
+{
+    public int Id { get; set; }
+    public string CityName { get; set; }
+    public string CountryName { get; set; }
+}
